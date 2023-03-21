d7e659ad748692dfcea41dc7d18730dbaa7a08a0
guoqing <1072543046@qq.com>
2023-03-21 15:42:39 +0800

    .

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,5 +2,8 @@ import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 
-@Module({controllers:[CoffeesController],providers:[CoffeesService]})
-export class CoffeesModule {}
+@Module({ 
+    controllers: [CoffeesController], 
+    providers: [CoffeesService] 
+})
+export class CoffeesModule { }
