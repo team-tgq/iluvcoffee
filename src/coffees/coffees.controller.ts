d2c897892d89fa2a1c09aa96526eb88ce2963672
guoqing <1072543046@qq.com>
2023-04-12 20:50:46 +0800

    .

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -7,25 +7,31 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
+import { Public } from 'src/common/decorators/public.decorator';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
+@UsePipes(ValidationPipe)
 @Controller('coffees')
 export class CoffeesController {
   constructor(
     /**告诉Nest将提供程序注入到控制器类中以便我们使用它 */
     private readonly coffeesService: CoffeesService,
   ) {}
+  @Public()
   @Get()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
   }
 
   @Get(':id')
-  findone(@Param('id') id: string) {
+  findone(@Param('id', ParseIntPipe) id: string) {
     return this.coffeesService.findOne(id);
   }
 
@@ -35,7 +41,10 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateCoffeeDto: UpdateCoffeeDto,
+  ) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +17,11 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
